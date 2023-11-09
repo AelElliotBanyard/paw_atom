ee70ee572f4b0f1684e08168bca812d3b5ce9f4f
Evan <evan.lueber@stud.kbw.ch>
2023-11-09 15:56:16 +0100

    styling unless done

--- atom/src/components/Contact.jsx
+++ atom/src/components/Contact.jsx
@@ -31,25 +31,16 @@ const Contact = () => {
       });
   };
   return (
-    <div>
-      <h1>Kontakt</h1>
+    <div className="flex flex-center justify-center items-center gap-8 flex-col bg-[#5c5c5cae] backdrop-blur-md rounded w-1/3 text-white">
+      <h1 className="text-3xl">Kontakt</h1>
       <div>
         <form ref={form} onSubmit={sendEmail}>
-          <div>
-            <div>
-              <div>
-                <label>Name</label>
-                <input type="text" name="user_name"/>
-              </div>
-              <div>
-                <label>Email</label>
-                <input type="email" name="user_email" />
-              </div>
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-row gap-2">
+                <input className=" bg-transparent border-b-2 border-white hover:border-blue-500 focus:outline-none" placeholder="Name" type="text" name="user_name"/>
+                <input className=" bg-transparent border-b-2 border-white hover:border-blue-500 focus:outline-none" placeholder="Email" type="email" name="user_email" />
             </div>
-            <div>
-              <label>Nachricht</label>
-              <textarea name="message" />
-            </div>
+              <textarea className=" bg-transparent border-b-2 border-white hover:border-blue-500 focus:outline-none" placeholder="Nachricht" name="message" />
           </div>
           <button type="submit" value="Send">
             Senden
