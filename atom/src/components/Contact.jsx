626040997a06b47749a6af4843b876c37bbd913f
Evan <evan.lueber@stud.kbw.ch>
2023-11-30 09:33:43 +0100

    gave contact id

--- atom/src/components/Contact.jsx
+++ atom/src/components/Contact.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
   };
   return (
     <div className="w-screen flex justify-center items-center">
-      <div className="flex w-1/2 self-center justify-center items-center gap-8 flex-col bg-[#5c5c5cae] backdrop-blur-md rounded text-white mb-32 shadow-purple-500 shadow-lg">
+      <div id="contact" className="flex w-1/2 self-center justify-center items-center gap-8 flex-col bg-[#5c5c5cae] backdrop-blur-md rounded text-white mb-32 shadow-purple-500 shadow-lg">
         <h1 className="text-3xl pt-5">Kontakt</h1>
         <div>
           <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-10">
