1a669312f1586db5385d235060419303cda77178
Evan <evan.lueber@stud.kbw.ch>
2023-11-30 13:13:48 +0100

    destroyed navbar

--- atom/src/App.jsx
+++ atom/src/App.jsx
@@ -439,74 +439,10 @@ function App() {
           Gleichgewichts des Schreckens in den internationalen Beziehungen bei.
         </p>
       </Section>
-      <Section
-        title="Folge von Einschlag einer Atombombe"
-        id="einschlagundfolgen"
-        side="right"
-        images={[
-          "https://upload.wikimedia.org/wikipedia/commons/f/fe/Hardtack_Umbrella_nuke.jpg",
-          "https://www.businessinsider.de/wp-content/uploads/2017/11/atompilz-nuclear-fallout-vorbereitung.jpg",
-        ]}
-      >
-        <p className="text-white">
-          Die Auswirkungen eines Atombombeneinschlags können äußerst verheerend
-          sein und lassen sich in vier Hauptkategorien Druckwelle,
-          Hitzestrahlung, radioaktive Strahlung und elektromagnetische Impulse
-          unterscheiden.
-        </p>
-        <br />
-        <p className="text-white">
-          Die erste unmittelbare Auswirkung eines Atombombeneinschlags ist die
-          Druckwelle, die durch die immense Hitze der Explosion erzeugt wird.
-          Diese Druckwelle hat das Potenzial, Gebäude und Strukturen zu
-          zerstören, und sie stellt eine unmittelbare Gefahr für Menschen dar,
-          da sie sie gegen Wände oder Objekte schleudern kann. Darüber hinaus
-          kann die Druckwelle auch zu erheblichen Gehörschäden führen.
-        </p>
-        <br />
-        <p className="text-white">
-          Die Hitzestrahlung entsteht aufgrund der immensen Hitze, die während
-          der Explosion freigesetzt wird. Sie kann lebensbedrohliche
-          Verbrennungen bei Menschen verursachen und hat auch das Potenzial,
-          Gebäude und andere Bauwerke durch Feuer zu zerstören.
-        </p>
-        <br />
-        <p className="text-white">
-          Die Freisetzung von radioaktiver Strahlung erfolgt ebenfalls durch die
-          Hitze während einer Explosion. Diese gefährliche Strahlung kann
-          Menschen töten, indem sie ihre Zellen schädigt und das Risiko für
-          Krebs erhöht. Zusätzlich besitzt sie die Fähigkeit, menschliche DNA zu
-          verändern, was zu schwerwiegenden gesundheitlichen Beeinträchtigungen
-          führen kann.
-        </p>
-        <br />
-        <p className="text-white">
-          Elektromagnetische Impulse entstehen aufgrund der extremen Hitze, die
-          bei einer Explosion freigesetzt wird. Diese Impulse haben das
-          Potenzial, elektronische Geräte zu zerstören und Stromausfälle zu
-          verursachen.
-        </p>
+      <Section title="quellen" id="quellen" side="left">
+        
       </Section>
-      <Section
-        title="Unterschied zu normalen Waffen im Aufbau"
-        id="differenzen"
-        side="left"
-        images={[
-          "https://upload.wikimedia.org/wikipedia/commons/f/fc/Implosionsdesign.svg",
-        ]}
-      >
-        <p className="text-white">
-          Jedem Menschen ist bewusst, dass eine Atomwaffen eine sehr grossen
-          Schaden anrichten kann. Doch was macht sie so zerstörerisch? Der
-          Aufbau einer Atomwaffe ist sehr komplex und besteht aus vielen
-          verschiedenen Komponenten. Eine Antom besteht aus einer Kugel aus
-          Plutonium-239 und aus einer Uran-235, in der sich eine Neutronenquelle
-          befindet. Die Kugel umgibt eine TNT schicht. Sobald das explodiert,
-          wird die Netronenquelle wirksam. Durch den Druck Explosion und das
-          Zusammendrücken der radioaktive Stoffen erreicht die Massedes
-          Spaltmaterials ihren kritischen Zustand.
-        </p>
-      </Section>
+      
       <div className="flex w-full justify-center items-center">
         <Contact />
       </div>
--- atom/src/components/Navbar.jsx
+++ atom/src/components/Navbar.jsx
@@ -1,50 +1,60 @@
 import React from "react";
 
 const Navbar = () => {
+  const [contentActive, setContentActive] = React.useState(false);
+  const [aboutActive, setAboutActive] = React.useState(false);
   return (
-    <div className=" bg-[#4C4C4C] rounded backdrop-blur-md p-5 flex flex-row justify-evenly items-center overflow-x-hidden shadow-purple-500 shadow-lg">
-      <a
-        href="#manhatten"
-        className="relative group text-2xl text-white px-2 py-2"
-      >
-        <span className="absolute bottom-0 left-0 top-0 w-0 rounded bg-white transition-all group-hover:w-full -z-10"></span>
-        <span className="group-hover:text-black">Manhattan-Project</span>
-      </a>
-      <a
-        href="#kernwaffen"
-        className="relative group text-2xl text-white px-2 py-2"
-      >
-        <span className="absolute bottom-0 left-0 top-0 w-0 rounded bg-white transition-all group-hover:w-full -z-10"></span>
-        <span className="group-hover:text-black">Kernwaffen</span>
-      </a>
-      <a
-        href="#besitz"
-        className="relative group text-2xl text-white px-2 py-2"
-      >
-        <span className="absolute bottom-0 left-0 top-0 w-0 rounded bg-white transition-all group-hover:w-full -z-10"></span>
-        <span className="group-hover:text-black">Besitz</span>
-      </a>
-      <a
-        href="#poliauswirkung"
-        className="relative group text-2xl text-white px-2 py-2"
-      >
-        <span className="absolute bottom-0 left-0 top-0 w-0 rounded bg-white transition-all group-hover:w-full -z-10"></span>
-        <span className="group-hover:text-black">Politische Auswirkung</span>
-      </a>
-      <a
-        href="#einschlagundfolgen"
-        className="relative group text-2xl text-white px-2 py-2 "
-      >
-        <span className="absolute bottom-0 left-0 top-0 w-0 rounded bg-white transition-all group-hover:w-full -z-10"></span>
-        <span className="group-hover:text-black">Einschlag und Folgen</span>
-      </a>
-      <a
-        href="#differenzen"
-        className="relative group text-2xl text-white px-2 py-2"
-      >
-        <span className="absolute bottom-0 left-0 top-0 w-0 rounded bg-white transition-all group-hover:w-full -z-10"></span>
-        <span className="group-hover:text-black">Differenzen</span>
-      </a>
+    <div className=" flex flex-col gap-2">
+      <div className=" bg-[#4C4C4C] rounded opacity-95 text-white text-3xl backdrop-blur-md p-5 flex flex-row justify-evenly items-center overflow-x-hidden shadow-purple-500 shadow-lg">
+        <p className="hover:text-purple-500 group/inhalt">Inhalt</p>
+        <p>Über</p>
+      </div>
+      <div className=" flex bg-[#4C4C4C] rounded text-white text-3xl opacity-95 backdrop-blur-md p-5 flex-row justify-evenly items-center overflow-x-hidden shadow-purple-500 shadow-lg">
+          <div className="group-hover/inhalt:scale-100 scale-0">
+            <a
+              href="#manhatten"
+              className="relative group text-2xl text-white px-2 py-2"
+            >
+              Manhattan-Projekt
+            </a>
+            <a
+              href="#kernwaffen"
+              className="relative group text-2xl text-white px-2 py-2"
+            >
+              Kernwaffen
+            </a>
+            <a
+              href="#besitz"
+              className="relative group text-2xl text-white px-2 py-2"
+            >
+              Besitzer
+            </a>
+            <a
+              href="#poliauswirkung"
+              className="relative group text-2xl text-white px-2 py-2"
+            >
+              Politische Auswirkung
+            </a>
+            <a
+              href="#einschlagundfolgen"
+              className="relative group text-2xl text-white px-2 py-2 "
+            >
+              Einschlag & Folgen
+            </a>
+            <a
+              href="#differenzen"
+              className="relative group text-2xl text-white px-2 py-2"
+            >
+              Differenzen
+            </a>
+          </div>
+        {aboutActive && (
+          <div>
+            <a href="#quellen">Quellen</a>
+            <a href="#contact"> Kontakt</a>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
