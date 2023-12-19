55b07593996993fcba28fb8e56c5c65f3cde86cc
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2023-12-19 10:44:54 +0100

    abbildungsverzeichnis started

--- next_atom/src/app/sources/page.jsx
+++ next_atom/src/app/sources/page.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h1>Abbildungen</h1>
+        <p>
+          Flagge USA:{" "}
+          <a href="https://images.rawpixel.com/image_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L3BkbWlzYzJiYXRjaDItbG9jOTc1MTU1NDktaW1hZ2VfNS5qcGc.jpg">
+            https://images.rawpixel.com/image_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L3BkbWlzYzJiYXRjaDItbG9jOTc1MTU1NDktaW1hZ2VfNS5qcGc.jpg
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default page;
