d95a19c4603b4623284154611f0d239246fd893a
Evan <evan.lueber@stud.kbw.ch>
2023-12-19 11:55:03 +0100

    abbildungen sources done

--- next_atom/src/app/sources/page.jsx
+++ next_atom/src/app/sources/page.jsx
@@ -2,100 +2,120 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <div>
-        <h1>Abbildungen</h1>
+    <div className="flex flex-col justify-center items-center bg-main bg-fixed">
+      <div className="p-10 bg-black bg-opacity-50 backdrop-blur-md w-2/3 break-words flex flex-col gap-4 mb-10 mt-10">
+        <p className="w-full text-center text-white text-5xl pb-10">Abbildungen</p>
         <p>
-          Flagge USA:{" "}
-          <a href="https://images.rawpixel.com/image_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L3BkbWlzYzJiYXRjaDItbG9jOTc1MTU1NDktaW1hZ2VfNS5qcGc.jpg">
+          Flagge USA: 
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://images.rawpixel.com/image_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L3BkbWlzYzJiYXRjaDItbG9jOTc1MTU1NDktaW1hZ2VfNS5qcGc.jpg">
             https://images.rawpixel.com/image_social_landscape/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA4L3BkbWlzYzJiYXRjaDItbG9jOTc1MTU1NDktaW1hZ2VfNS5qcGc.jpg
           </a>
         </p>
         <p>
-          Flagge Russland:{" "}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/4/40/Российский0_флаг1200px-Flag_of_Russia.png">
+          Flagge Russland:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/4/40/Российский0_флаг1200px-Flag_of_Russia.png">
             https://upload.wikimedia.org/wikipedia/commons/4/40/Российский0_флаг1200px-Flag_of_Russia.png
           </a>
         </p>
         <p>
-          Flagge China:{" "}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/China_Flag_-_Alternative.svg/1024px-China_Flag_-_Alternative.svg.png?20160821181037">
+          Flagge China:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/China_Flag_-_Alternative.svg/1024px-China_Flag_-_Alternative.svg.png?20160821181037">
             https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/China_Flag_-_Alternative.svg/1024px-China_Flag_-_Alternative.svg.png?20160821181037
           </a>
         </p>
         <p>
-          Flagge UK:{" "}
-          <a href="https://www.publicdomainpictures.net/pictures/340000/nahled/national-flag-of-uk-the-united-kingdom-of-great-britain-and-no-1588673083tIa.jpg">
+          Flagge UK:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://www.publicdomainpictures.net/pictures/340000/nahled/national-flag-of-uk-the-united-kingdom-of-great-britain-and-no-1588673083tIa.jpg">
             https://www.publicdomainpictures.net/pictures/340000/nahled/national-flag-of-uk-the-united-kingdom-of-great-britain-and-no-1588673083tIa.jpg
           </a>
         </p>
         <p>
-          Flagge Frankreich:{" "}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_France_%28new%29.png">
+          Flagge Frankreich:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_France_%28new%29.png">
             https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_France_%28new%29.png
           </a>
         </p>
         <p>
-          Flagge Nordkorea:{" "}
-          <a href="https://cdn.britannica.com/47/1947-004-44F4E790/Flag-North-Korea.jpg">
+          Flagge Nordkorea:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://cdn.britannica.com/47/1947-004-44F4E790/Flag-North-Korea.jpg">
             https://cdn.britannica.com/47/1947-004-44F4E790/Flag-North-Korea.jpg
           </a>
         </p>
         <p>
-          Flagge Indien:{" "}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/7/7b/India_flag_300.png">
+          Flagge Indien:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/7/7b/India_flag_300.png">
             https://upload.wikimedia.org/wikipedia/commons/7/7b/India_flag_300.png
           </a>
         </p>
         <p>
-          Flagge Pakistan:{" "}
-          <a href="https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg">
+          Flagge Pakistan:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg">
             https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg
           </a>
         </p>
         <p>
-          Flagge Israel:{" "}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/5/5c/Israel_flag_300.png">
+          Flagge Israel:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/5/5c/Israel_flag_300.png">
             https://upload.wikimedia.org/wikipedia/commons/5/5c/Israel_flag_300.png
           </a>
         </p>
         <p>
-          Implosionsdesign: {""}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/f/fc/Implosionsdesign.svg">
+          Implosionsdesign:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/f/fc/Implosionsdesign.svg">
             https://upload.wikimedia.org/wikipedia/commons/f/fc/Implosionsdesign.svg
           </a>
         </p>
         <p>
-          Grösse Der Waffen: {""}
-          <a href="https://cdn10.picryl.com/photo/1987/01/01/a-graph-depicting-soviet-and-nato-longer-range-intermediate-range-nuclear-missiles-4e2c19-1024.jpg">
+          Grösse Der Waffen:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://cdn10.picryl.com/photo/1987/01/01/a-graph-depicting-soviet-and-nato-longer-range-intermediate-range-nuclear-missiles-4e2c19-1024.jpg">
             https://cdn10.picryl.com/photo/1987/01/01/a-graph-depicting-soviet-and-nato-longer-range-intermediate-range-nuclear-missiles-4e2c19-1024.jpg
           </a>
         </p>
         <p>
-          Leslie Groves:{" "}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/3/37/Leslie_Groves.jpg">
+          Leslie Groves:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/3/37/Leslie_Groves.jpg">
             https://upload.wikimedia.org/wikipedia/commons/3/37/Leslie_Groves.jpg
           </a>
         </p>
         <p>
-          Oppenheimer:{" "}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/8/85/Oppenheimer_%28cropped%29.jpg">
+          Oppenheimer:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/8/85/Oppenheimer_%28cropped%29.jpg">
             https://upload.wikimedia.org/wikipedia/commons/8/85/Oppenheimer_%28cropped%29.jpg
           </a>
         </p>
         <p>
-          Enrico Fermi:{" "}
-          <a href="https://upload.wikimedia.org/wikipedia/commons/d/d4/Enrico_Fermi_1943-49.jpg">
+          Enrico Fermi:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://upload.wikimedia.org/wikipedia/commons/d/d4/Enrico_Fermi_1943-49.jpg">
             https://upload.wikimedia.org/wikipedia/commons/d/d4/Enrico_Fermi_1943-49.jpg
           </a>
         </p>
         <p>
-          Explosionsvergleich: {""}
-          <a href="https://pbs.twimg.com/media/FMVfj0QVQAsKJdw.jpg">https://pbs.twimg.com/media/FMVfj0QVQAsKJdw.jpg</a>
+          Explosionsvergleich:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://pbs.twimg.com/media/FMVfj0QVQAsKJdw.jpg">
+            https://pbs.twimg.com/media/FMVfj0QVQAsKJdw.jpg
+          </a>
         </p>
         <p>
-          Radiusvergleich: {""}
-          <a href="https://www.aasc.ucla.edu/cab/imgContent/200708230003.jpg">https://www.aasc.ucla.edu/cab/imgContent/200708230003.jpg</a>
+          Radiusvergleich:
+          <br />
+          <a className="text-blue-500 border-b border-b-blue-500" href="https://www.aasc.ucla.edu/cab/imgContent/200708230003.jpg">
+            https://www.aasc.ucla.edu/cab/imgContent/200708230003.jpg
+          </a>
         </p>
       </div>
     </div>
