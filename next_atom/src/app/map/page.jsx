242e8195c3a0dbcc02f12f44c06a263b625d93cc
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2023-12-19 08:04:14 +0100

    Made landingpage

--- next_atom/src/app/map/page.jsx
+++ next_atom/src/app/map/page.jsx
@@ -1,35 +1,33 @@
-'use client'
-import React, { useEffect } from 'react';
+"use client";
+import React, { useEffect } from "react";
 import WorldMap from "react-svg-worldmap";
 
 const page = () => {
-
-  const data = [
-    { country: "us", value: 5400 },
-    { country: "ru", value: 6000 },
-    { country: "cn", value: 400 },
-    { country: "uk", value: 120 },
-    { coutnry: "fr", value: 290 },
-    { country: "kp", value: 50 },
-    { country: "in", value: 160 },
-    { country: "pk", value: 170 },
-    { country: "il", value: 100 }
-  ];
   let size = 200;
   useEffect(() => {
     size = Math.min(window.innerWidth, window.innerHeight) * 1.2;
   }, []);
   return (
-    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
       <WorldMap
         color="lightblue"
-        backgroundColor='black'
-        borderColor='white'
+        backgroundColor="black"
+        borderColor="white"
         value-suffix="Atomwaffen"
-        valuePrefix='~'
-        data={data}
+        valuePrefix="~"
+        data={[
+          { country: "us", value: 5400 },
+          { country: "ru", value: 6000 },
+          { country: "cn", value: 400 },
+          { country: "uk", value: 120 },
+          { coutnry: "fr", value: 290 },
+          { country: "kp", value: 50 },
+          { country: "in", value: 160 },
+          { country: "pk", value: 170 },
+          { country: "il", value: 100 },
+        ]}
         size={size}
-    />
+      />
     </div>
   );
 };
--- next_atom/src/components/Navigation.jsx
+++ next_atom/src/components/Navigation.jsx
@@ -7,21 +7,73 @@ const Navigation = () => {
   const [showNav, setShowNav] = React.useState(false);
 
   return (
-    <div className={" flex justify-center items-center absolute top-0 right-0 " + (showNav === true ? " w-full h-screen z-30 " : "")}>
-        <div className={" absolute top-0 right-0 transition-transform m-2 z-10 " + (showNav === true ? " rotate-90 " : " rotate-0")} onClick={(e) => setShowNav(!showNav)}>
-          <TiThMenu size={48} />
-        </div>
+    <div
+      className={
+        " flex justify-center items-center fixed top-0 right-0 z-50 " +
+        (showNav === true ? " w-full h-screen z-30 " : "")
+      }
+    >
+      <div
+        className={
+          " absolute top-0 right-0 transition-transform m-2 z-10 bg-gray-300 bg-opacity-20 rounded " +
+          (showNav === true ? " rotate-90 " : " rotate-0")
+        }
+        onClick={(e) => setShowNav(!showNav)}
+      >
+        <TiThMenu size={48} />
+      </div>
 
-        <div className={" h-full flex justify-center items-center w-full backdrop-blur-md bg-gray-300 bg-opacity-50  transition-transform origin-top-right " + (showNav === true ? "scale-100": " scale-0")}>
-          <div className=" w-1/3 flex flex-col justify-center items-center gap-10 text-3xl font-bold text-center">
-            <Link className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full" onClick={(e) => setShowNav(false)} href="/">Startseite</Link>
-            <Link className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full" onClick={(e) => setShowNav(false)} href="/nuc">Kernwaffen</Link>
-            <Link className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full" onClick={(e) => setShowNav(false)} href="/effects">Auswirkungen</Link>
-            <Link className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full" onClick={(e) => setShowNav(false)} href="/map">Karte</Link>
-            <Link className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full" onClick={(e) => setShowNav(false)} href="/contact">Kontakt</Link>
-            <Link className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full" onClick={(e) => setShowNav(false)} href="/sources">Quellen</Link>
-          </div>
+      <div
+        className={
+          " h-full flex justify-center items-center w-full backdrop-blur-md bg-gray-300 bg-opacity-50  transition-transform origin-top-right " +
+          (showNav === true ? "scale-100" : " scale-0")
+        }
+      >
+        <div className=" w-1/3 flex flex-col justify-center items-center gap-10 text-3xl font-bold text-center">
+          <Link
+            className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full"
+            onClick={(e) => setShowNav(false)}
+            href="/"
+          >
+            Startseite
+          </Link>
+          <Link
+            className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full"
+            onClick={(e) => setShowNav(false)}
+            href="/nuc"
+          >
+            Kernwaffen
+          </Link>
+          <Link
+            className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full"
+            onClick={(e) => setShowNav(false)}
+            href="/effects"
+          >
+            Auswirkungen
+          </Link>
+          <Link
+            className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full"
+            onClick={(e) => setShowNav(false)}
+            href="/map"
+          >
+            Karte
+          </Link>
+          <Link
+            className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full"
+            onClick={(e) => setShowNav(false)}
+            href="/contact"
+          >
+            Kontakt
+          </Link>
+          <Link
+            className="hover:bg-[#cc070e] hover:bg-opacity-50 hover:rounded p-2 w-full"
+            onClick={(e) => setShowNav(false)}
+            href="/sources"
+          >
+            Quellen
+          </Link>
         </div>
+      </div>
     </div>
   );
 };
