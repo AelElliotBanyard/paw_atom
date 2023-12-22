ef594e3a20e8e550c11de734197184f0a705bb26
Ael Elliot Banyard <64694151+AelElliotBanyard@users.noreply.github.com>
2023-12-22 21:22:29 +0000

    another small errror

--- next_atom/next.config.js
+++ next_atom/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
-}
+  distDir: "build",
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
